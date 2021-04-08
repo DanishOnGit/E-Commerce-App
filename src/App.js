3e08b07cbd7aed4531f6e23a88f9af2456e652da
Danish Ahmed <65118743+DanishOnGit@users.noreply.github.com>
2021-04-08 20:53:25 +0530

    fix: dependency eslint error

--- src/App.js
+++ src/App.js
@@ -57,7 +57,8 @@ export default function App() {
         console.log(err);
       }
     })();
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ ]);
 
   return (
     <div className="App">
