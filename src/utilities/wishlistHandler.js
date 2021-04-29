808a21bf63372340192c48f1d41f988cfff5a854
Danish Ahmed <65118743+DanishOnGit@users.noreply.github.com>
2021-04-30 02:09:33 +0530

    feat: backend added

--- src/utilities/wishlistHandler.js
+++ src/utilities/wishlistHandler.js
@@ -1,51 +1,56 @@
 import axios from "axios";
 import { checkIfAlreadyPresent } from "../utilities";
 
-export async function wishlistHandler(wishlistItems, dispatch, item,showToast,setIsDisabled,isRendered) {
+export async function wishlistHandler(
+  wishlistItems,
+  dispatch,
+  item,
+  showToast,
+  setIsDisabled,
+  isRendered
+) {
   try {
-   
-    const returnedValue = checkIfAlreadyPresent(wishlistItems, item.id);
+    const returnedValue = checkIfAlreadyPresent(wishlistItems, item._id);
+
     if (!returnedValue) {
-      showToast(`Adding ${item.brand} to wishlist...`,"info");
+      showToast(`Adding ${item.brand} to wishlist...`, "info");
       setIsDisabled(true);
-      const response = await axios.post("./api/wishlistItems", {
-        wishlistItem: {
-          ...item,
-          existsInWishlist: true
+      const response = await axios.post(
+        "https://Badminton-ecomm.danishahmed27.repl.co/wishlist",
+        {
+          _id: item._id
         }
-      });
+      );
 
       if (response.status === 201) {
-        showToast(`Added!`,"success");
+        showToast(`Added!`, "success");
         dispatch({
-          type: "ADD_TO_WISHLIST",
-          payload: response.data.wishlistItem
+          type: "GET_WISHLIST_ITEMS",
+          payload: response.data.wishlistItem.wishlistItems
         });
       }
     } else {
-      const response = await axios.put(
-        `./api/wishlistItems/${returnedValue.id}`,
+      const response = await axios.post(
+        `https://Badminton-ecomm.danishahmed27.repl.co/wishlist`,
+
         {
-          wishlistItem: {
-            ...item,
-            existsInWishlist: !returnedValue.existsInWishlist
-          }
+          _id: item._id
         }
       );
+
       if (response.status === 200) {
-        dispatch({ type: "ADD_TO_WISHLIST", payload: returnedValue });
+        dispatch({
+          type: "GET_WISHLIST_ITEMS",
+          payload: response.data.wishlistItem.wishlistItems
+        });
       }
     }
   } catch (err) {
-    console.log(err);
-    showToast(`Could not add ${item.brand} to wishlist!`,"failure")
-
-  }
-  finally{
-
-    if (isRendered) { 
-      setIsDisabled(false)
+    console.log("Wisholist post eror is...", err);
+    showToast(`Could not add ${item.brand} to wishlist!`, "failure");
+  } finally {
+    if (isRendered) {
+      setIsDisabled(false);
     }
-
   }
 }
