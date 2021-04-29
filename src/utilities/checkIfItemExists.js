c151222c622b2644cef9a0f49b4d011608fd5b98
Danish Ahmed <65118743+DanishOnGit@users.noreply.github.com>
2021-04-30 02:08:32 +0530

    feat : backend added

--- src/utilities/checkIfItemExists.js
+++ src/utilities/checkIfItemExists.js
@@ -1,4 +1,4 @@
 export const checkIfAlreadyPresent = (array, itemId) => {
-  const isPresent = array.find((product) => product.id === itemId);
+  const isPresent = array.find((product) => product?.productId?._id === itemId);
   return isPresent;
 };
