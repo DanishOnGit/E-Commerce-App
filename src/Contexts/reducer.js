b61d4080133bd64bdd58615dcfa9b9f274b5b72a
DanishOnGit <youcandoit1911@gmail.com>
2021-07-19 20:24:06 +0530

    fix:clear filter

--- src/Contexts/reducer.js
+++ src/Contexts/reducer.js
@@ -56,6 +56,7 @@ export function reducer(state, action) {
       return {
         ...state,
         sortBy: null,
+        filterBy:{categories:[]},
         priceRangeControl: 5000,
         showFastDeliveryOnly: false,
         showFullInventory: true,
